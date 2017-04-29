1d6f59830ed098f4d73c041a707d64c4ae67cbd8
Jorge Padilla Velasco <jorgepadilla.060995@gmail.com>
2017-04-28 23:21:10 -0500

    Update MetodoGaussSeidel.cpp

--- MetodoGaussSeidel.cpp
+++ MetodoGaussSeidel.cpp
@@ -2,41 +2,92 @@
 #include <cmath>
 using namespace std;
 
-double fx(double y, double z) {
-    
-    return (7 + y - z)/2;
-}
 
-double fy(double x, double z) {
-    
-    return (6 - x + z)/2;
-}
+void Seidel(int size){
+    double EPSILON = 0.1; // error iterativo como tolerancia
+    double A[size][size];
+    double B[size];
+    double S[size]; //Array with the solutions
+    double OS[size];  //Array with old solutions
+    double E[size]; //Array with th error of each variable
+    cout<<"Enter the right hand side constants : " << endl;
+  for(int i = 0; i < size; i++) {
+      cin >> B[i];
+  }
+  cout<<"Enter the coefficients row wise : " << endl;
+  for(int i = 0; i < size; i++) {
+      for(int j = 0; j < size; j++) {
+          cin>>A[i][j];
+      }
+  }
+    for (int i = 0; i < size; ++i)
+    {
+        S[i] = 0;
+        OS[i] = -999999;
+        E[i] = (S[i]-OS[i]); // error absoluto
+        //E[i] = ((S[i]-OS[i])/S[i]); // error relativo
+        //E[i] = ((S[i]-OS[i])/S[i]) * 100 ; // error porcentual
 
-double fz(double x, double y) {
-    
-    return 12 - x - y;
-}
-
-int main() {
-    
-    double x, y, z;
-    double error = pow(10, -12);
-    int i;
-    x = y = z = 0.0;
-    
-    do {
-        
-        x = fx(y, z);
-        y = fy(x, z);
-        z = fz(x, y);
-        i++;
-        cout << x << " | " << y << " | " << z << " | " << i << endl;
     }
-    while(abs(x + y + z - 12)>error || abs(2*x - y + z - 7)>error || abs(x + 2*y - z - 6)>error);
-    
-    cout << "x = " << x << endl;
-    cout << "y = " << y << endl;
-    cout << "z = " << z << endl;
-    cout << "i = " << i << endl;
 
+    bool errorCheck = false;
+    for (int i = 0; i < size; ++i)
+    {
+        if(E[i] <= EPSILON){
+            errorCheck = true;
+            break;
+        }
+    }
+
+    int counter=0;
+
+    while(!errorCheck){
+        for (int i = 0; i < size; ++i)
+        {
+            double sum = 0;
+            for (int j = 0; j < size; ++j)
+            {
+                if (j != i)
+                {
+                    sum += A[i][j]*S[j];
+                }
+            }
+            OS[i] = S[i];
+            S[i] = (B[i] - sum)/A[i][i];
+
+            E[i] = (S[i]-OS[i]);
+
+
+        }
+
+        for (int i = 0; i < size; ++i)
+        {
+            if(abs(E[i]) > EPSILON){
+                break;
+            }
+            errorCheck = true;
+        }
+        counter++;
+    }
+
+    cout << "The Solution is : " << endl;
+    for (int i = 0; i < size; ++i)
+    {
+        cout<<"x("<<i<<") = "<<S[i]<<"\n";
+    }
+}
+
+int main(){
+    int size;
+    int solutions[] = {7,6,12};
+
+    int n, l;
+
+    float x [n];
+    cout<<"Enter the value of n : " << endl;
+    cin>>n;
+    Seidel(n);
+    //cout<<"The Solution is : " << endl;
+
+    return 0;
 }
